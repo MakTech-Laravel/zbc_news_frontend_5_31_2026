60d7dc41f0dc80980490e4ca2f0ef7041ea1d15b
dev-sumon <akhtaruzzamansumon7@gmail.com>
2026-06-06 18:11:04 +0600

    some update

--- src/pages/frontend/Entertainment.tsx
+++ src/pages/frontend/Entertainment.tsx
@@ -3,13 +3,12 @@ import { FeaturedSection } from "@/components/main-layout/content/FeaturedSectio
 import { HeroSection } from "@/components/main-layout/content/HeroSection";
 import { LatestStories } from "@/components/main-layout/content/LatestStories";
 import { latestStories } from "@/data/dummy/home";
-import { heroArticle } from "@/data/dummy/entertainment";
 
 
 export default function Entertainment() {
   return (
     <article className="flex flex-col gap-5 sm:gap-7 lg:gap-8">
-      <HeroSection article={heroArticle} />
+      <HeroSection  />
       <AdUnit variant="banner" />
       <FeaturedSection />
       <AdUnit variant="banner" />
