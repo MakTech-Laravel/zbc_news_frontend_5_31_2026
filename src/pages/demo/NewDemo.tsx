258ab761e40b9cd7597d485ac1c87e16bbe69bdb
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2026-06-22 15:31:59 +0600

    Wrap NewDemo component in Layout for improved structure and consistency.

--- src/pages/demo/NewDemo.tsx
+++ src/pages/demo/NewDemo.tsx
@@ -1,5 +1,9 @@
+import { Layout } from "@/components/Layout";
+
 export default function NewDemo() {
   return (
-    <div>NewDemo</div>
-  )
+    <Layout>
+      <div>NewDemo</div>
+    </Layout>
+  );
 }
